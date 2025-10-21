6c6357b5366bf505ff548ef89551101872986112
rintaro <rinsa0211@gmail.com>
2025-10-21 02:37:27 +0200

    commit all file

--- composeApp/src/commonMain/kotlin/org/junia/friendbook/uiAboutAccount/screens/about/AboutScreen.kt
+++ composeApp/src/commonMain/kotlin/org/junia/friendbook/uiAboutAccount/screens/about/AboutScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.painterResource
 import friendbook.composeapp.generated.resources.Res
 import friendbook.composeapp.generated.resources.about_page
+import org.jetbrains.compose.ui.tooling.preview.Preview
 
 /**
  * About screen (single page)
@@ -28,6 +29,7 @@ import friendbook.composeapp.generated.resources.about_page
  *  - Any header outside this screen is NOT included here.
  */
 
+@Preview
 @Composable
 fun AboutScreen() {
     Scaffold { padding ->
